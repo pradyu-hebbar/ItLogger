3b1be24e6a1859d6c13124f1dc78b337e2610719
Pradyumna Hebbar <pradyu.hebbar@gmail.com>
2021-08-11 13:58:26 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { Fragment, useEffect } from "react";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
+import AddBtn from "./components/layout/AddBtn";
+import AddLogModal from "./components/logs/AddLogModal";
+import EditLogModal from "./components/logs/EditLogModal";
+import { Provider } from "react-redux";
+import store from "./store";
+import AddTechModal from "./components/techs/AddTechModal";
+import TechListModal from "./components/techs/TechListModal";
+const App = () => {
+  useEffect(() => {
+    // Init Materialize JS
+    M.AutoInit();
+  }, []);
+  return (
+    <Provider>
+      <Fragment>
+        <SearchBar />
+        <div className='container'>
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
+  );
+};
+
+export default App;
